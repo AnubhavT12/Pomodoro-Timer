46a26419ba2d545545ea17bc21210ebd2e9a12c9
Anubhav Thakur <anybhav12@gmail.com>
2026-07-04 12:19:32 +0530

    completed button clicking events

---
+++ script.js
@@ -0,0 +1,14 @@
+const timerCont = document.querySelector(".timer");
+const btnCOnt = document.querySelector(".btn-cont");
+const playBtn = document.querySelector(".play");
+const pauseBtn = document.querySelector(".pause");
+const Minutes = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+
+
+console.log(timerCont);
+console.log(btnCOnt);
+
+
+
+playBtn.addEventListener
